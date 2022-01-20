33d08f3e97b5e7a373e64cdb2a7d1bfbfd1c3060
trip6love <lanebyers13@gmail.com>
2022-01-20 15:50:36 -0500

    adding the const to the indexjs file

---
+++ index.js
@@ -0,0 +1,12 @@
+// PAGE CREATE //
+const generateHTML = require('./src/generateHTML');
+
+// PROFILES //
+const Engineer = require('./lib/Engineer');
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+
+// NODE //
+const inquirer = require('inquirer');
+const fs = require('fs');
+
