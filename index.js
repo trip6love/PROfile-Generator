59cd83b20fb79a8f7c5fdfa53b571619d2b3c09c
trip6love <lanebyers13@gmail.com>
2022-01-20 16:01:55 -0500

    adding manager prompt to indexJS

--- index.js
+++ index.js
@@ -10,3 +10,39 @@ const Intern = require('./lib/Intern');
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+// ARRAY //
+const dbArray = [];
+
+// MANAGER PROMPTS //
+const dbManager = () => {
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Who is the manager?', 
+            validate: nameEntry => {
+                if (nameEntry) {
+                    return true;
+                } else {
+                    console.log ("Please enter a name!");
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Please enter your email.",
+            validate: email => {
+                valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+                if (valid) {
+                    return true;
+                } else {
+                    console.log ('Please enter your email!')
+                    return false; 
+                }
+            }
+        },
+        
+    ])
+}
