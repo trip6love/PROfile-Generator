13d272ed1439cab14b4ea2327377eec7e1dd628d
trip6love <lanebyers13@gmail.com>
2022-01-20 23:29:55 -0500

    added shell for each card

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,13 +1,38 @@
 // MANAGER CARD //
-
+const genManager = function (manager) {
+    return `
+    <div class="col-4 mt-4">
+    `;
+}
 
 //INTERN CARD //
+const genIntern = function (intern) {
+    return `
 
+    `;
+}
 
 // ENGINEER CARD //
+const genEngineer = function (engineer) {
+    return `
+
+    `;
+}
 
 
 // PUSH ARRAY TO PAGE //
+genPage = (data) => {
+
+    pArray = [];
+
+
+}
 
 
 // GENERATE PAGE //
+const genHTML = function () {
+    
+}
+
+
+module.exports = generateHTML;
